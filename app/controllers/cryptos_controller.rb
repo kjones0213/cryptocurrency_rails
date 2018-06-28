6f4eab3f69439d8c62ea2a89e400287afb44ec97
Kristen Jones <krisjones0213@gmail.com>
2018-06-28 18:58:19 -0500

    Tweaks the crypto show page

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -22,6 +22,14 @@ class CryptosController < ApplicationController
   # GET /cryptos/1
   # GET /cryptos/1.json
   def show
+    @cryptos = Crypto.all
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @show_crypto = JSON.parse(@response)
   end
 
   # GET /cryptos/new
